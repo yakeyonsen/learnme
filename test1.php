0529f0ce47b1d4bbf3ff92c532d014b95eec275d
yakeyonsen <54915481+yakeyonsen@users.noreply.github.com>
2019-09-26 21:42:52 -0600

    Add files via upload
    test php file

---
+++ test1.php
@@ -0,0 +1,7 @@
+<?php // test1.UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1
+ $username = "Fred Smith" ;
+ echo $username;
+ echo "<br>";
+ $current_user = $username;
+ echo $current_user;
+ ?>
